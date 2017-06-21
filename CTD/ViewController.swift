66ce11892245814faea2e738d9da88c3b8fc95fc
Jay Chhaidan <Jay.Chhaidan@hotmail.com>
2017-06-21 12:46:58 -0400

    Initial Commit

---
+++ CTD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CTD
+//
+//  Created by Jason Chhaidan on 2017-06-21.
+//  Copyright © 2017 New User Experience. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CTDTests/CTDTests.swift
@@ -0,0 +1,36 @@
+//
+//  CTDTests.swift
+//  CTDTests
+//
+//  Created by Jason Chhaidan on 2017-06-21.
+//  Copyright © 2017 New User Experience. All rights reserved.
+//
+
+import XCTest
+@testable import CTD
+
+class CTDTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
